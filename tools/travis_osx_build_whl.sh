082075da89f3d7275d9ae47d39c23e949f3e789d
Zhongchuan Sun <ZhongchuanSun@users.noreply.github.com>
2020-06-13 00:01:31 +0800

    Update travis_osx_build_whl.sh

--- tools/travis_osx_build_whl.sh
+++ tools/travis_osx_build_whl.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+python -m pip install cibuildwheel==1.4.2
+
 python -m pip install -r requirements.txt
-python setup.py sdist bdist_wheel
+# python setup.py sdist bdist_wheel
+python -m cibuildwheel --output-dir wheelhouse
